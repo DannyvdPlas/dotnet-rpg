b999e106618cbd3ea916255f59dea6dbed349876
Danny <s1096011@student.hsleiden.nl>
2022-12-12 11:18:26 +0100

    Alles in één

---
+++ Data/DataContext.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        // Dit is een dataseeder. Normaal gewoon mvc patroon gebruiken voor crud stuff
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill {Id = 1, Name = "Fireball", Damage = 30},
+                new Skill {Id = 2, Name = "Frenzy", Damage = 20},
+                new Skill {Id = 3, Name = "Blizzard", Damage = 50}
+            );
+        }
+        // Einde dataseeder
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
